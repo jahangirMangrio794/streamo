f607aacd7273410536e2aeca247d98acd8fe6fa5
jahangirMangrio794 <jamjahangeerr@gmail.com>
2026-05-25 08:07:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("STREAMO website loaded successfully!");
+document.addEventListener("DOMContentLoaded", function () {
+
+  const loginBtn = document.querySelector(".right button");
+  loginBtn.addEventListener("click", function () {
+    alert("Login system abhi backend se connect nahi hai!");
+  });
+
+  const playBtn = document.querySelector(".play");
+  playBtn.addEventListener("click", function () {
+    alert("Watch Now clicked!");
+  });
+
+  const listBtn = document.querySelector(".list");
+  listBtn.addEventListener("click", function () {
+    alert("Added to My List!");
+  });
+
+});
